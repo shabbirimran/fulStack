3ab791df291f76dc1a30aef3e68c7655c10b96a1
Shabbir <shabbir.ali818@gmail.com>
2024-08-27 22:52:16 +0500

    cookies spelling mistake

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -2,17 +2,18 @@ import jwt from "jsonwebtoken";
 import { Apierror } from "../utils/Apierror.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/User.Models.js";
-export const verifyJWT=asyncHandler(async(req,res,next)=>{
+export const verifyJWT=asyncHandler(async(req,_,next)=>{
    try {
-     const token=req.cookie?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+     const token=req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+    console.log(req.cookies?.accessToken,"token")
      if(!token){
-         return new Apierror(401,"Unauthorized request")
+         throw new Apierror(401,"Unauthorized request")
      }
      const decodeToken= jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
  const user = await User.findById(decodeToken?._id).select("-password -refreshToken")
      console.log(user)
  if(!user){
-     return new Apierror(401,"Invalid Access Token")
+     throw new Apierror(401,"Invalid Access Token")
  }
  req.user=user
  next()
